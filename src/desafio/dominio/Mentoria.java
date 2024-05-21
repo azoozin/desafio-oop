c636c854715e04434fe721104ddca4c88db01c4b
azoozin <mario.github@protonmail.com>
2024-05-21 00:26:36 -0300

    add class Mentoria

---
+++ src/desafio/dominio/Mentoria.java
@@ -0,0 +1,45 @@
+package desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    // private int cargaHoraria;
+    private LocalDate data;
+
+    // getters setters
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
